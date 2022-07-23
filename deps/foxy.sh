aeba7ce329f5d699a31bf274d293bfe768e9525b
k-nish <koh.oct.un@gmail.com>
2022-07-23 17:58:22 +0900

    [add] update cuda version & ROS gpg key

--- deps/foxy.sh
+++ deps/foxy.sh
@@ -1,6 +1,8 @@
 #/usr/bin/env bash
 
 export ROS_DISTRO=foxy
+export LANG=en_US.UTF-8
+export INSTALL_ROS_KEY=C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends --allow-unauthenticated \
@@ -17,30 +19,23 @@ sudo apt-get install -y --no-install-recommends --allow-unauthenticated \
      libccd-dev \
      python3-pykdl \
      doxygen \
-     redis-tools \
-     redis-server \
-    && apt-get autoclean \
-    && apt-get autoremove
-
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-# setup keys
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-# setup sources.list
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
-
-# install ros2 packages
-sudo apt-get update
-
-sudo apt-get install -y --no-install-recommends ros-${ROS_DISTRO}-desktop \
+    && sudo apt-get autoclean \
+    && sudo apt-get autoremove \
+    && sudo locale-gen en_US en_US.UTF-8 \
+    && sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 \
+    && sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu focal main" > /etc/apt/sources.list.d/ros2-latest.list' \
+    && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${INSTALL_ROS_KEY} \
+    && sudo apt update \
+    && sudo apt-get install -y --no-install-recommends ros-${ROS_DISTRO}-desktop \
      ros-${ROS_DISTRO}-cv-bridge \
-     ros-${ROS_DISTRO}-ament-*
-
-sudo pip3 install pytest --upgrade
-sudo apt-get install -y python3-colcon-common-extensions \
-     python3-vcstool
+     ros-${ROS_DISTRO}-ament-* \
+     python3-colcon-common-extensions \
+     python3-colcon-mixin \
+     python3-rosdep \
+     python3-vcstool \
+    && sudo pip3 install pytest --upgrade \
+    && sudo apt-get install -y python3-colcon-common-extensions \
+         python3-vcstool \
+    && rosdep init || true && rosdep update --rosdistro ${ROS_DISTRO}
 
 echo "\033[33mInstalled ROS ${ROS_DISTRO} \033[0m"
